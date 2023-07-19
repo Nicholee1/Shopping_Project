0eaa0d2d0f3cbfe34eea884057a3589375169703
leech122 <2367570034@qq.com>
2023-07-19 08:08:19 +0800

    add oauth2 ResourceConfig

---
+++ shopping/src/main/java/org/learning/shopping/config/ResourceConfig.java
@@ -0,0 +1,41 @@
+package org.learning.shopping.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+
+@Configuration
+@EnableResourceServer
+public class ResourceConfig extends ResourceServerConfigurerAdapter {
+/*    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }*/
+
+   @Primary
+    @Bean
+    public RemoteTokenServices remoteTokenServices(){
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setCheckTokenEndpointUrl("http://localhost:8606/oauth/check_token");
+        tokenServices.setClientId("test");
+        tokenServices.setClientSecret("test");
+        return tokenServices;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+       resources.tokenServices(remoteTokenServices());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
